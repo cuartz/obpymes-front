ed1a27e8efa14334a02cf891bb63654927cd583f
DoJoker <jessloar@gmail.com>
2016-08-12 09:42:28 -0500

    función general para peticiones ajax

--- app/scripts/controllers/requirements.js
+++ app/scripts/controllers/requirements.js
@@ -9,27 +9,25 @@
 var app = angular.module('ob-pymes');
 
 function RequirementsCtrl($scope, api) {
-    //debugger;
     var vm = this;
-
-    api.callService('/mensajes?id=0', 'GET', function (data) {
-        vm.condicion = data;
+    api.callService('/mensajes?id=0', 'GET',1).then(function(d) {
+      vm.condicion = d;
     });
-
-    api.callService('/mensajes?id=1', 'GET', function (data) {
-        vm.documento1 = data;
+    api.callService('/mensajes?id=1', 'GET',300).then(function(d) {
+        vm.documento1 = d;
 	});
-	api.callService('/mensajes?id=2', 'GET', function (data) {
-		vm.documento2 = data;
+	api.callService('/mensajes?id=2', 'GET',400).then(function(d) {
+		vm.documento2 = d;
 	});
-	api.callService('/mensajes?id=3', 'GET', function (data) {
-		vm.documento3 = data;
+	api.callService('/mensajes?id=3', 'GET',400).then(function(d) {
+		vm.documento3 = d;
 	});
 	$scope.$parent.deshabilitado = false;
 	$scope.$parent.nextPath = '/formulario';
 }
 
-app.controller('RequirementsCtrl', RequirementsCtrl );
+app.controller('RequirementsCtrl', ['$scope', 'ConsumeRestService', RequirementsCtrl ]);
 
-RequirementsCtrl.$inject = ['$scope', 'ConsumeRestService'];
+
+
 
