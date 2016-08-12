22301bdbb08fe2503a1b184f7d5148440ea0eeb0
AlainCarmona <alaincarmonahdz@gmail.com>
2016-08-12 11:45:31 -0500

    12-08-2016
    Se corrige el consumo de servicios desde generalServices.js

--- app/scripts/controllers/requirements.js
+++ app/scripts/controllers/requirements.js
@@ -5,26 +5,26 @@
  * @description
  * # Requirements
  * Controller of the ob-pymes
-*/
+ */
 var app = angular.module('ob-pymes');
 
 function RequirementsCtrl($scope, api) {
     var vm = this;
-    
+
     api.callService('/requirements/conditions', 'GET').then(function (data) {
         vm.conditions = data.body;
     });
-    
+
     api.callService('/requirements/documents', 'GET').then(function (data) {
         vm.documents = data.body;
     });
-    
+
     api.callService('/requirements/days', 'GET').then(function (data) {
-	vm.days = data.body.days;
+        vm.days = data.body;
     });
-    
-    $scope.$parent.deshabilitado=false;
+
+    $scope.$parent.deshabilitado = false;
     $scope.$parent.nextPath = '/formulario';
 }
 
-app.controller('RequirementsCtrl', ['$scope', 'ConsumeRestService', RequirementsCtrl ]);
+app.controller('RequirementsCtrl', ['$scope', 'ConsumeRestService', RequirementsCtrl]);
--- app/scripts/services/generalServices.js
+++ app/scripts/services/generalServices.js
@@ -6,19 +6,19 @@
  * # generalServices
  * Service of the ob-pymes
  */
- var app = angular.module('generalServices',[]);
+var app = angular.module('generalServices', []);
 
- app.factory('ConsumeRestService', ['$http', '$q', '$timeout', function ($http, $q, $timeout) {
-    var promise;
-    var serviceAjaxRequest =  {
-        callService: function (url, method, minTimeTransaction, headers) {
-            if ( !promise ) {
+app.factory('ConsumeRestService', ['$http', '$q', '$timeout', function ($http, $q, $timeout) {
+        var promise;
+        var serviceAjaxRequest = {
+            callService: function (url, method, minTimeTransaction, headers) {
+//            if ( !promise ) {
                 var minTimeRequest = minTimeTransaction || 50000;
 
                 var timeOutPromise = $timeout(function () {
-                     canceler.resolve();
-                     console.log('Time out: Se revaso el tiempo de espera configurado para ' + url + ' tiempo '+ minTimeRequest +' limite = ' + minTimeRequest);
-                 }, minTimeRequest);
+                    canceler.resolve();
+                    console.log('Time out: Se revaso el tiempo de espera configurado para ' + url + ' tiempo ' + minTimeRequest + ' limite = ' + minTimeRequest);
+                }, minTimeRequest);
 
                 var canceler = $q.defer();
                 var status = 'Request submited';
@@ -28,18 +28,18 @@
                     'url': 'http://localhost:8080' + url,
                     'headers': headers || {},
                     'timeout': canceler.promise
-                }).then(function(response) {
+                }).then(function (response) {
                     var endTime = (new Date()).getTime();
                     status = 'Respuesta correcta en  ' + (endTime - startTime) + ' msec. Para: ' + url;
                     console.log(status);
                     $timeout.cancel(timeOutPromise);
                     return response.data;
                 });
+//            }
+                return promise;
             }
-            return promise;
-        }
-    };
-    return serviceAjaxRequest;
-}]);
+        };
+        return serviceAjaxRequest;
+    }]);
 
 
